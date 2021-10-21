86e5c44b1cae094eedbe9f40b471dfeb1307220e
hrantm <hrant.mirijanyan@gmail.com>
2021-10-21 16:59:47 +0400

    add gorilla/mux

--- main.go
+++ main.go
@@ -5,9 +5,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type Article struct {
+	Id      string `json:"Id"`
 	Title   string `json:"Title"`
 	Desc    string `json:"desc"`
 	Content string `json:"content"`
@@ -16,9 +19,26 @@ type Article struct {
 var Articles []Article
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/articles", returnAllArticles)
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", returnAllArticles)
+	myRouter.HandleFunc("/articles/{id}", returnSingleArticle)
+	log.Fatal(http.ListenAndServe(":3000", myRouter))
+}
+
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+	var result Article
+	for _, article := range Articles {
+		if article.Id == key {
+			result = article
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(result)
+
 }
 
 func returnAllArticles(w http.ResponseWriter, r *http.Request) {
@@ -33,8 +53,9 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	Articles = []Article{
-		Article{Title: "Hello", Desc: "Description", Content: "Article Content"},
-		Article{Title: "Hello2", Desc: "desc 2", Content: "Content 2"},
+		Article{Id: "1", Title: "Hello", Desc: "Description", Content: "Article Content"},
+		Article{Id: "2", Title: "Hello2", Desc: "desc 2", Content: "Content 2"},
 	}
+	fmt.Println("Starting Server")
 	handleRequests()
 }
